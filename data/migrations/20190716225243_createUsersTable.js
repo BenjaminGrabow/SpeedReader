fac272bce83bdd9295b85266a56832dff11ddd05
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-18 19:36:08 +0200

    Feature: added text in migrate file back

--- data/migrations/20190716225243_createUsersTable.js
+++ data/migrations/20190716225243_createUsersTable.js
@@ -1,11 +1,19 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('memoryGame', table => {
+exports.up = function (knex) {
+  return knex.schema.createTable('texts', table => {
     table.increments();
-    table.text('frontPicture', 100).notNullable();
-    table.text('backPicture', 100).notNullable();
-  });
+    table.text('text', 10000).notNullable();
+    table.text('name').notNullable()
+  })
+    .createTable('memoryGame', table => {
+      table.increments();
+      table.text('frontPicture', 100).notNullable();
+      table.text('backPicture', 100).notNullable();
+    })
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('memoryGame');
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('texts')
+    .dropTableIfExists('memoryGame');
 };

d6832041166b76056230ceaf693fe590763ca319
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-18 22:34:01 +0200

    Feature: added StyledTextAdder file

--- speedreader/src/Components/ReadFastText/TextSection/TextAdder.js
+++ speedreader/src/Components/ReadFastText/TextSection/TextAdder.js
@@ -1,73 +1,8 @@
 import React from 'react';
+import StyledTextAdder from './StyledTextAdder';
 import { connect } from 'react-redux';
 import { addText, chooseThisText, deleteText } from '../../../Store/actions';
 import axios from 'axios';
-import styled from 'styled-components';
-
-const StyledTextAdder = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-color: white;
-
-.all-texts {
-  display: flex;
-  flex-wrap: wrap;
-
-.text {
-  width: 10rem;
-  height: 8rem;
-  background: white;
-  color: black;
-  text-align: center;
-  margin: 1rem;
-
-  .close {
-  display: flex;
-  justify-content: flex-end;
-  width: 100%;
-
-  i {
-    font-size: 2rem;
-
-    &:hover {
-      color: red;
-    }
-  }
-}
-
-p {
-  width: 100%;
-  height: 100%;
-}
-}
-}
-
-.input-plus {
-display: flex;
-justify-content: space-around;
-align-items: center;
-width: 30%;
-}
-
-i {
-  font-size: 4rem;
-  transition: .6s ease-in-out;
-
-  &:hover {
-    color: black;
-  }
-}
-
-input {
-font-size: 1.5rem;
-text-align: center;
-width: 70%;
-height: 25rem;
-border-radius: .5rem;
-}
-`;
 
 class TextAdder extends React.Component {
   constructor(props) {

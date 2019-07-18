878c2c4c9cdbd9bea3503a0d1deb5bdabde6f6a3
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-18 20:36:18 +0200

    Feature: added new StyledFindWords file

---
+++ speedreader/src/Components/FindWords/StyledFindWords.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+const StyledFindWords = styled.div`
+
+.words-to-find {
+display: flex;
+width: 100%;
+justify-content: space-around;
+
+p {
+  color: white;
+  font-size: 1.5rem;
+}
+}
+
+.search-words {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  }
+
+.no {
+    height: 3rem;
+    width: 5%;
+    margin: 0;
+    border: .1rem solid white;
+    text-align: center;
+  p {
+    width: 100%;
+    height: 100%;
+    font-weight: bolder;
+    font-size: 1.5rem;
+    color: white;
+    margin: 0;
+  }
+}
+
+.found {
+    width: 100%;
+    height: 100%;
+    font-weight: bolder;
+    font-size: 1.5rem;
+    color: black;
+    background-color: red;
+}
+
+.user-won {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+`;
+
+export default StyledFindWords;

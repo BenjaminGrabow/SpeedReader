d7f4c056595df7f0f0dfa01756f61951482c59e3
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-24 17:19:06 +0200

    Feature: added reducer methods to reducer.js

--- speedreader/src/Store/reducer.js
+++ speedreader/src/Store/reducer.js
@@ -1,6 +1,9 @@
 import * as types from './actions';
 
 const initialState = {
+  loading: false,
+  error: null,
+  loggingIn: false,
   savedTexts: [],
   currentText: [],
   textIfPause: [],
@@ -10,6 +13,18 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case types.REGISTER:
+      return { ...state };
+
+    case types.LOGIN_START:
+      return { ...state, loggingIn: true };
+
+    case types.LOGIN_SUCCESS:
+      return { ...state, loggingIn: false };
+
+    case types.LOGIN_FAIL:
+      return { ...state, loggingIn: false, error: action.payload }
+
     case types.FETCH_TEXTS:
       const copyOfFetchedTexts = action.fetchedTexts;
 

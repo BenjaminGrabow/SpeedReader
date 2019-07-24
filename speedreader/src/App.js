fe8fac26de19d10bee0f0e04de1d3333548a8d97
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-24 23:14:04 +0200

    Feature: added image to homepage component

--- speedreader/src/App.js
+++ speedreader/src/App.js
@@ -11,13 +11,15 @@ import styled from 'styled-components';
 import './App.css';
 
 const StyledDiv = styled.div`
-background-image: linear-gradient(to right, rgba(0,0,0,.5), rgba(0,0,0,.5)),url(${books});
+/* background-image: linear-gradient(to right, rgba(0,0,0,.5), rgba(0,0,0,.5)),url(${books});
 background-position: center;
 background-repeat: no-repeat; 
 background-size: cover;
-height: 100vh;
+height: 100vh; */
 `;
 
+
+// ADD LOCALSTORAGE FOR FREE USERS !!!
 function App() {
   return (
     <StyledDiv>
--- speedreader/src/Components/HomePage/HomePage.js
+++ speedreader/src/Components/HomePage/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import book from './book.jpg';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -6,14 +7,26 @@ const StyledHomePage = styled.div`
 
 header {
   display: flex;
-justify-content: space-between;
+justify-content: space-around;
+align-items: center;
 /* background-color: #FFFFFF; */
 
 h1 {
   font-family: 'Permanent Marker', cursive;
-  font-size: 3rem;
-  color: white;
+  /* font-size: 3rem; */
+  color: #F15A29;
 }
+
+a {
+  text-decoration: none;
+  color: black;
+  font-weight: 600;
+}
+}
+
+img {
+  width: 100%;
+  height: 30rem;
 }
 `;
 
@@ -22,9 +35,10 @@ const HomePage = () => {
   <StyledHomePage>
     <header>
     <h1>SpeedReader</h1>
-  <NavLink to="/account">Account</NavLink>
-  <NavLink to="/text">Main App</NavLink>
+ <NavLink to="/account">Account</NavLink>
+  <NavLink to="/text">App</NavLink>
   </header>
+  <img src={book} alt="book on grass"/>
   </StyledHomePage>
   );
 }

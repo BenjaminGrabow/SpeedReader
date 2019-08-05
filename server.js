8614b9d856001d1eebc60a38120e8dcf0205b6d4
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-05 14:02:52 +0200

    Feature: added new route for user preference

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const authRouter = require('./routes/auth-router.js');
-// const usersRouter = require('./users/users-router.js');
 const textRouter = require('./routes/text-router.js');
 const findWordRouter = require('./routes/find-word-router.js');
 const memoryRouter = require('./routes/memory-router.js');
@@ -15,7 +14,6 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/', authRouter);
-// server.use('/users', usersRouter);
 server.use('/', textRouter);
 server.use('/', findWordRouter);
 server.use('/', memoryRouter);

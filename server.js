5bb0d9816a38f563ee623d37adb64f3e8b222f12
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-05 14:04:20 +0200

    Feature: import new route for user preference in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const authRouter = require('./routes/auth-router.js');
+const userPreferenceRouter = require('./routes/user-preference-router.js');
 const textRouter = require('./routes/text-router.js');
 const findWordRouter = require('./routes/find-word-router.js');
 const memoryRouter = require('./routes/memory-router.js');
@@ -14,6 +15,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/', authRouter);
+server.use('/', userPreferenceRouter);
 server.use('/', textRouter);
 server.use('/', findWordRouter);
 server.use('/', memoryRouter);
